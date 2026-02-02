a6f2252d94901a81cd94695c0d3e9cbdcbbbc9fb
Nathan Romano <romano.nathan54@gmail.com>
2026-02-02 18:56:46 +0100

    add usememo dor counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import './App.scss';
 import Cell from './components/Cell/Cell';
 import SquareCounter from './components/SquareCounter/SquareCounter';
@@ -6,7 +6,6 @@ import SquareCounter from './components/SquareCounter/SquareCounter';
 function App() {
   const cellCount = 10
 
-  const [counter, setCounter] = useState<number>(0)
   const [isChecked, setIsChecked] = useState<boolean[]>(Array(cellCount).fill(false))
 
   const changeState = (index: number) => {
@@ -15,30 +14,28 @@ function App() {
     setIsChecked(table)
   }
 
-  const handleCounter = () => {
+  const squareCount = useMemo(() => {
     let squareNumber: number = 0
     for (const value of isChecked) {
       if (value === true) {
         squareNumber += 1
       }
     }
-    setCounter(squareNumber)
-  }
+    return squareNumber
+  }, [isChecked]);
 
   return (
-    <>
-      <div className="app">
-        <h1>Where Picross</h1>
-        <div className='board'>
-          <div className="grid">
-            {isChecked.map((value: boolean, index: number)=> 
-              <Cell isChecked={value} onInteract={() => {changeState(index); handleCounter()}}/>
-            )}
-          </div>
-          <SquareCounter counter={counter}/>
+    <div className="app">
+      <h1>Where Picross</h1>
+      <div className='board'>
+        <div className="grid">
+          {isChecked.map((value: boolean, index: number)=> 
+            <Cell isChecked={value} onInteract={() => {changeState(index);}}/>
+          )}
         </div>
+        <SquareCounter counter={squareCount}/>
       </div>
-    </>
+    </div>
   )
 }
 
