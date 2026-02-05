7b6fb78f8ec12eddae139a7e358f983aad1077e3
Nathan Romano <romano.nathan54@gmail.com>
2026-02-05 16:04:22 +0100

    add solution marker, wip refactor counter function

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import Grid from './components/Grid/Grid';
 import SquareCounter from './components/SquareCounter/SquareCounter';
 
 function App() {
-  const cellCount = 6
-  const lineCount = 6
 
-  const [stateGrid, setStateGrid] = useState<boolean[][]>(Array(lineCount).fill(Array(cellCount).fill(false)))
+  const solutionGrid = [
+    [true, true, false, true, true, true],
+    [true, true, false, false, true, true],
+    [false, true, false, false, false, true],
+    [false, false, false, true, true, true,],
+    [false, true, false, true, true, true,],
+    [false, true, false, true, true, true,],
+  ]
+
+  const startState: boolean[][] = Array(solutionGrid.length).fill(Array(solutionGrid[0].length).fill(false))
+
+  const [stateGrid, setStateGrid] = useState<boolean[][]>(startState)
 
   const changeState = (line: number, cell: number) => {
     const grid = [...stateGrid]
@@ -21,9 +30,14 @@ function App() {
     <div className="app">
       <h1>Where Picross</h1>
       <div className='board'>
+        <div className='vertical-counter-wrapper'>
+          <SquareCounter stateGrid={solutionGrid} isXAxis={false}/>
+        </div>
         <div className='horizontal-wrapper'>
+          <div className='horizontal-counter-wrapper'>
+            <SquareCounter stateGrid={solutionGrid} isXAxis={true}/>
+          </div>
           <Grid stateGrid={stateGrid} onCellClick={changeState}/>
-
           <div className='horizontal-counter-wrapper'>
             <SquareCounter stateGrid={stateGrid} isXAxis={true}/>
           </div>
--- src/components/SquareCounter/SquareCounter.tsx
+++ src/components/SquareCounter/SquareCounter.tsx
@@ -10,36 +10,31 @@ function SquareCounter({ stateGrid, isXAxis }: CounterProps) {
 
     const squareCount = useCallback((index: number, isXAxis:boolean) => {
         const sequence: number[] = []
+        const forIterate: number = isXAxis ? stateGrid[0].length : stateGrid.length
         let squareNumber: number = 0
         let previousState: boolean | null  = null
-        let forIterate: number = 0
-        let currentChecked: boolean| null = null
-        if (isXAxis) {
-            forIterate = stateGrid[0].length
-            
-        } else {
-            forIterate = stateGrid.length
-        }
+        let currentChecked: boolean | null = null
 
         for (let i = 0; i < forIterate; i += 1) {
             if (isXAxis) {
-                currentChecked = stateGrid[index][i]
+                currentChecked = stateGrid[index][i] 
             } else {
                 currentChecked = stateGrid[i][index]
             }
 
-        if (currentChecked) {
-            squareNumber += 1
-        } else {
-            if (previousState) {
-            sequence.push(squareNumber)
+            if (currentChecked) {
+                squareNumber += 1
+            } else {
+                if (previousState) {
+                sequence.push(squareNumber)
+                }
+                squareNumber = 0
             }
-            squareNumber = 0
+            previousState = currentChecked
         }
-        previousState = currentChecked
-        }
+        
         if (squareNumber != 0) {
-        sequence.push(squareNumber)
+            sequence.push(squareNumber)
         }
         return sequence.join("-")
     }, [stateGrid]);
