2284bdc98947752ef74b7f9ffadfe120c16c9652
Nathan Romano <romano.nathan54@gmail.com>
2026-02-12 16:59:48 +0100

    add multiple schema

---
+++ src/components/utils/picross-schema.ts
@@ -0,0 +1,73 @@
+
+// 5 x 5
+const croix5x5 = [
+    [false, false, true,  false, false],
+    [false, false, true,  false, false],
+    [true,  true,  true,  true,  true ],
+    [false, false, true,  false, false],
+    [false, false, true,  false, false],
+]
+
+const coeur5x5 = [
+    [true,  false, false, false, true ],
+    [true,  true,  false, true,  true ],
+    [true,  true,  true,  true,  true ],
+    [false, true,  true,  true,  false],
+    [false, false, true,  false, false],
+]
+
+const smiley5x5 = [
+    [false, true,  false, true,  false],
+    [false, true,  false, true,  false],
+    [false, false, false, false, false],
+    [true,  false, false, false, true ],
+    [false, true,  true,  true,  false],
+]
+
+// 7 x 7
+const diamant7x7 = [
+    [false, false, false, true,  false, false, false],
+    [false, false, true,  true,  true,  false, false],
+    [false, true,  true,  true,  true,  true,  false],
+    [true,  true,  true,  true,  true,  true,  true ],
+    [false, true,  true,  true,  true,  true,  false],
+    [false, false, true,  true,  true,  false, false],
+    [false, false, false, true,  false, false, false],
+]
+
+const maison7x7 = [
+    [false, false, true,  true,  true,  false, false],
+    [false, true,  true,  true,  true,  true,  false],
+    [true,  true,  true,  true,  true,  true,  true ],
+    [false, false, true,  true,  true,  false, false],
+    [false, false, true,  true,  true,  false, false],
+    [false, false, true,  true,  true,  false, false],
+    [true,  true,  true,  true,  true,  true,  true ],
+]
+
+// 10 x 10
+const coeur10x10 = [
+    [false, true,  true,  false, false, false, false, true,  true,  false],
+    [true,  true,  true,  true,  false, false, true,  true,  true,  true ],
+    [true,  true,  true,  true,  true,  true,  true,  true,  true,  true ],
+    [true,  true,  true,  true,  true,  true,  true,  true,  true,  true ],
+    [false, true,  true,  true,  true,  true,  true,  true,  true,  false],
+    [false, false, true,  true,  true,  true,  true,  true,  false, false],
+    [false, false, false, true,  true,  true,  true,  false, false, false],
+    [false, false, false, false, true,  true,  false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+]
+
+const etoile10x10 = [
+    [false, false, false, true,  false, false, true,  false, false, false],
+    [false, false, false, true,  false, false, true,  false, false, false],
+    [true,  true,  true,  true,  true,  true,  true,  true,  true,  true ],
+    [false, false, true,  true,  true,  true,  true,  true,  false, false],
+    [false, false, false, true,  true,  true,  true,  false, false, false],
+    [true,  true,  true,  true,  true,  true,  true,  true,  true,  true ],
+    [false, false, false, true,  true,  true,  true,  false, false, false],
+    [false, false, true,  true,  true,  true,  true,  true,  false, false],
+    [false, false, false, true,  false, false, true,  false, false, false],
+    [false, false, false, true,  false, false, true,  false, false, false],
+]
