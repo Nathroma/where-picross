96163e4ff21befbf9fde060061e716bb325815e9
Nathan Romano <romano.nathan54@gmail.com>
2026-02-16 15:30:00 +0100

    add a playtime stopwatch

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -12,14 +12,17 @@ type GamePageProps = {
 function GamePage({picross, returnToMenu: returnToMainMenu}: GamePageProps) {
 
     const [timer, setTimer] = useState<number>(0)
+    const [isFinished, setIsFinished] = useState<boolean>(false)
 
     useEffect(()=> {
-        const interval = setInterval(() => {
-            setTimer(timer => timer + 1)
-        }, 1000)
-
-        return () => clearTimeout(interval)
-    }, [])
+        if (!isFinished) {
+            const interval = setInterval(() => {
+                setTimer(timer => timer + 1)
+            }, 1000)
+            return () => clearTimeout(interval)
+        }
+       
+    }, [isFinished])
 
     const solutionGrid = useMemo(() => picross.grid, [])
     const startState: boolean[][] = Array(solutionGrid.length).fill(Array(solutionGrid[0].length).fill(false))
@@ -33,18 +36,34 @@ function GamePage({picross, returnToMenu: returnToMainMenu}: GamePageProps) {
       grid[line] = currentLine
       setCurrentGrid(grid)
     }
-  
-    const isComplete = () => JSON.stringify(currentGrid) === JSON.stringify(solutionGrid)
+
+    useMemo(() => {
+        if (JSON.stringify(currentGrid) === JSON.stringify(solutionGrid)) {
+            setIsFinished(true)
+        }
+    }, [currentGrid, solutionGrid])
 
     const formattedTimer = () => {
-        const minute: number = Math.floor(timer / 60)
-        const seconde: string = String(timer % 60).padStart(2, "0")
-        return `${minute}:${seconde}`
+        const rawHour: number = Math.floor(timer / 3600)
+        const rawMinute: number = Math.floor(timer / 60) % 60
+        const rawSecond: number = timer % 60
+
+        if (rawHour === 0) {
+            const formattedMinute: string = String(rawMinute)
+            const formattedSecond: string = String(rawSecond).padStart(2, "0")
+            return `${formattedMinute}:${formattedSecond}`
+        } else {
+            const formattedHour: string = String(rawHour)
+            const formattedMinute: string = String(rawMinute).padStart(2, "0")
+            const formattedSecond: string = String(rawSecond).padStart(2, "0")
+            return `${formattedHour}:${formattedMinute}:${formattedSecond}`
+        }
     }
 
     return (
         <div className="game-page">
-            <h2>{formattedTimer()}</h2>
+            {isFinished && <h2>Félicitation !</h2>}
+            <p>{formattedTimer()}</p>
             <div className='board'>
                 <div className='vertical-counter-wrapper'>
                     <CounterBlock solutionGrid={solutionGrid} currentGrid={currentGrid} isXAxis={false}/>
